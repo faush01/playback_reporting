bf5b8a2a84e4f9dedcee58706e3b425ebf90289e
shaun <shaun@bluebit.com.au>
2018-03-25 19:20:26 +1100

    switch to chart js lib

--- emby_user_stats/Pages/config.js
+++ emby_user_stats/Pages/config.js
@@ -1,69 +1,82 @@
 ﻿define([], function () {
     'use strict';
 
-    function draw_graph(context, local_d3) {
+    function draw_graph(context, local_chart) {
 
-        if (!local_d3) {
-            alert("No D3 : " + local_d3);
+        if (!local_chart) {
+            alert("No Chart : " + local_d3);
             return;
         }
 
-        var svg = local_d3.select("svg");
-        var margin = { top: 20, right: 20, bottom: 30, left: 50 };
-        var width = +svg.attr("width") - margin.left - margin.right;
-        var height = +svg.attr("height") - margin.top - margin.bottom;
-        var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var barChartData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'Dataset 1',
+                backgroundColor: '#FF0000',
+                data: [
+                    10,
+                    20,
+                    30,
+                    40,
+                    50,
+                    60,
+                    70
+                ]
+            }, {
+                label: 'Dataset 2',
+                backgroundColor: '#0000FF',
+                data: [
+                    10,
+                    20,
+                    30,
+                    40,
+                    50,
+                    60,
+                    70
+                ]
+            }, {
+                label: 'Dataset 3',
+                backgroundColor: '#00FF00',
+                data: [
+                    10,
+                    20,
+                    30,
+                    40,
+                    50,
+                    60,
+                    70
+                ]
+            }]
 
-        var parseTime = local_d3.timeParse("%Y-%m-%d");
+        };
 
-        var x = local_d3.scaleTime()
-            .rangeRound([0, width]);
-
-        var y = local_d3.scaleLinear()
-            .rangeRound([height, 0]);
-
-        var line = local_d3.line()
-            .x(function (d) { return x(d.Date); })
-            .y(function (d) { return y(d.Count); });
-
-        local_d3.json("/emby/user_usage_stats/all/30/Activity", function (error, data) {
-            if (error) throw error;
-
-            for (var index = 0; index < data.length; ++index) {
-                data[index].Date = parseTime(data[index].Date)
-                console.log("Graph Data : " + data[index].Date + " - " + data[index].Count);
+        var ctx = document.getElementById('user_stats_chart_canvas').getContext('2d');
+        window.myBar = new Chart(ctx, {
+            type: 'bar',
+            data: barChartData,
+            options: {
+                title: {
+                    display: true,
+                    text: 'Chart.js Bar Chart - Stacked'
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false
+                },
+                responsive: true,
+                scales: {
+                    xAxes: [{
+                        stacked: true,
+                    }],
+                    yAxes: [{
+                        stacked: true
+                    }]
+                }
             }
-
-            x.domain(local_d3.extent(data, function (d) { return d.Date; }));
-            y.domain(local_d3.extent(data, function (d) { return d.Count; }));
-
-            g.append("g")
-                .attr("transform", "translate(0," + height + ")")
-                .call(local_d3.axisBottom(x))
-                .select(".domain")
-                .remove();
-
-            g.append("g")
-                .call(local_d3.axisLeft(y))
-                .append("text")
-                .attr("fill", "#000")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", "0.71em")
-                .attr("text-anchor", "end")
-                .text("Play Counts");
-
-            g.append("path")
-                .datum(data)
-                .attr("fill", "none")
-                .attr("stroke", "steelblue")
-                .attr("stroke-linejoin", "round")
-                .attr("stroke-linecap", "round")
-                .attr("stroke-width", 1.5)
-                .attr("d", line);
-
-            console.log("Graph Done");
         });
+
+        console.log("Chart Done");
+
     }
 
     return function (view, params) {
@@ -72,7 +85,7 @@
 
         view.addEventListener('viewshow', function (e) {
 
-            require([Dashboard.getConfigurationResourceUrl('d3.v4.min.js')], function (d3) {
+            require(['https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js'], function (d3) {
                 draw_graph(view, d3);
             });
 
